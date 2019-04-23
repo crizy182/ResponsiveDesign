5e45159eef441ac1a7b670dfcb725c8137db842f
cristopher <cristhiam.tovar@mail.escuelaing.edu.co>
2019-04-23 12:16:45 -0500

    defecto de iconos

--- script/buger.js
+++ script/buger.js
@@ -1,8 +1,20 @@
-console.log('esto sirve de verdad');
-console.log(document.querySelector('.menu').classList.add('is-active'));
-
+const ipad = window.matchMedia('screen and (max-width: 767px)');
 const menu = document.querySelector('.menu');
 const burgerButton = document.querySelector('#burger-menu');
-burgerButton.addEventListener('click', () => {
-  menu.classList.toggle('is-active');
-});
+
+ipad.addListener(validation);
+//se activa boton menu burger para la primera vez que se ingresa
+function validation(event) {
+  if (event.matches) burgerButton.addEventListener('click', showHide);
+  else burgerButton.removeEventListener('click', showHide);
+}
+validation(ipad);
+if (ipad.matches) burgerButton.addEventListener('click', showHide);
+
+function showHide() {
+  if (menu.classList.contains('is-active')) {
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active');
+  }
+}
